fb1daeb5ecef7c86a7345ca990210810dd04afc6
Kevin Ahlstrom <ikeavin@gmail.com>
2019-02-11 14:47:52 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,5 @@ $("#animator").click(function() {
     $("#mainHeading").animate({fontSize: "+=40px"}, 1500);
     $("#mainHeading").animate({fontSize: "-=40px"}, 1500);
     $("#kev_image").addClass("borderClass");
+    $("#mainHeading").css("color", "black");
 });
