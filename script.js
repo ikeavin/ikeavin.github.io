34b3c987e76cf1e7988ed1bdef2f1b19476845df
Kevin Ahlstrom <ikeavin@gmail.com>
2019-02-11 09:35:37 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 $("#animator").click(function() {
     $("#mainHeading").css("color", "#DA70D6");
+    $("#mainHeading").animate({fontSize: "150%"}, 1500);
+    $("#mainHeading").animate({fontSize: "100%"}, 1500);
 });
